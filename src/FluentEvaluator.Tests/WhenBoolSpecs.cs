c4a1eaf8a9800998a2c88d7bf7cd6d19256dcd14
sbiefeld <sbiefeld@4eda550c-0b76-11de-b321-f573fdaff03e>
2009-03-12 07:01:59 +0000

    added tests for when just passing in a boolean value

---
+++ src/FluentEvaluator.Tests/WhenBoolSpecs.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using SpecUnit;
+
+namespace FluentEvaluator.Tests
+{
+	public class WhenBoolSpecs : ContextSpecification
+	{
+	}
+
+	[TestFixture]
+	[Concern("when(bool)")]
+	public class when_passing_in_true : ContextSpecification
+	{
+		private int _myNumber = 11;
+
+		protected override void Context()
+		{
+			When.This(true).DoThis(() => _myNumber++).Engage();
+		}
+
+		[Test]
+		[Observation]
+		public void should_make_my_number_equal_twelve()
+		{
+			_myNumber.ShouldEqual(12);
+		}
+	}
+
+	[TestFixture]
+	[Concern("when(bool)")]
+	public class when_passing_in_false: ContextSpecification
+	{
+		private int _myNumber = 11;
+
+		protected override void Context()
+		{
+			When.This(false).DoThis(() => _myNumber++).Engage();
+		}
+
+		[Test]
+		[Observation]
+		public void should_leave_my_number_as_eleven()
+		{
+			_myNumber.ShouldEqual(11);
+		}
+	}
+}
