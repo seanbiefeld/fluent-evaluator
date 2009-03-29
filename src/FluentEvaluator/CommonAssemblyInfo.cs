f634ec3e66b2c8098f97870862d39650382fa61a
sbiefeld <sbiefeld@4eda550c-0b76-11de-b321-f573fdaff03e>
2009-03-29 06:50:55 +0000

    setting things up for nant build

---
+++ src/FluentEvaluator/CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyVersionAttribute("0.4.1.35")]
+[assembly: AssemblyFileVersionAttribute("0.4.1.35")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Sean Biefeld 2008-2009")]
+[assembly: AssemblyProductAttribute("FluentEvaluator")]
+[assembly: AssemblyConfigurationAttribute("Debug")]
+[assembly: AssemblyInformationalVersionAttribute("0.4.1.35")]
